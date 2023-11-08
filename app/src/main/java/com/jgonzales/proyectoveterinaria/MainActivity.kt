144d873b016a9b9f616e984be989c9fe94bad583
jose <jose.gonzales.2084@gmail.com>
2023-11-07 21:17:07 -0500

    Formulario de registro agregado

--- app/src/main/java/com/jgonzales/proyectoveterinaria/MainActivity.kt
+++ app/src/main/java/com/jgonzales/proyectoveterinaria/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.jgonzales.proyectoveterinaria
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnCrearCuenta:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        inicializar()
+    }
+    fun inicializar(){
+        btnCrearCuenta = findViewById(R.id.btnRegistrarse)
+        btnCrearCuenta.setOnClickListener{
+            val intento1 = Intent(this, RegistroUsuario::class.java)
+            startActivity(intento1)
+        }
     }
 }
